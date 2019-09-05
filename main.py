7746261668da25ca1b8b762de966f8808d515c6b
Michael Beck <brit228@gmail.com>
2019-09-05 03:37:14 -0400

    Updated scheduler

--- main.py
+++ main.py
@@ -58,7 +58,6 @@ spec:
 
 def callback(message):
     msg = message.data.decode('utf-8')
-    logging.warning(msg)
     doc_ref = db.document(msg)
     doc_ref.update({
         "status": "processing"
@@ -73,21 +72,55 @@ def callback(message):
             "biopred-{}-job".format(msg.split('/')[-1].lower()),
             "biopred-prediction-job",
             "predict",
-            "gcr.io/biopred/github.com/brit228/biopred-prediction:9fc87f5",
+            "gcr.io/biopred/github.com/brit228/biopred-prediction:latest",
             msg
         ),
         verify=False
     )
-    logging.warning(r.text)
 
 subscriber = pubsub.SubscriberClient()
 sub_path = subscriber.subscription_path('biopred', 'pulljobs')
 
 while True:
-    time.sleep(10)
-    response = subscriber.pull(sub_path, max_messages=5)
+    time.sleep(1)
+    response = subscriber.pull(sub_path, max_messages=10)
     for msg in response.received_messages:
         callback(msg.message)
     ack_ids = [msg.ack_id for msg in response.received_messages]
     if len(ack_ids) > 0:
-      subscriber.acknowledge(sub_path, ack_ids)
+        subscriber.acknowledge(sub_path, ack_ids)
+    r = requests.get(
+        url,
+        headers={
+            'Authorization': 'Bearer '+ open('/var/run/secrets/kubernetes.io/serviceaccount/token','r').read().strip(),
+            'Content-Type': 'application/yaml'
+        },
+        verify=False
+    )
+    joblist = r.json()
+    for j in joblist['items']:
+        if j['status'].get('active', 0) == 0:
+            if j['status'].get('failed', 0) == 0:
+                r = requests.delete(
+                    url+'/'+j['metadata']['name'],
+                    headers={
+                        'Authorization': 'Bearer '+ open('/var/run/secrets/kubernetes.io/serviceaccount/token','r').read().strip(),
+                        'Content-Type': 'application/yaml'
+                    },
+                    data='gracePeriodSeconds: 0\norphanDependents: false\n',
+                    verify=False
+                )
+            else:
+                r = requests.delete(
+                    url+'/'+j['metadata']['name'],
+                    headers={
+                        'Authorization': 'Bearer '+ open('/var/run/secrets/kubernetes.io/serviceaccount/token','r').read().strip(),
+                        'Content-Type': 'application/yaml'
+                    },
+                    data='gracePeriodSeconds: 0\norphanDependents: false\n',
+                    verify=False
+                )
+                doc_ref = db.document(j['metadata']['name'].split('-')[1])
+                doc_ref.update({
+                    "status": "failed"
+                })
